8e4642e496651c19d5c7bfaf7fc26d7047704b39
Sayyid Marvan <sayyidmarvanvt@gmail.com>
2024-06-19 17:39:57 +0530

    responive

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -74,7 +74,7 @@ const Main = () => {
               </p>
               <p>How can I help you today?</p>
             </div>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-5">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 gap-4 p-5">
               {cards.map((card, index) => (
                 <div
                   key={index}
